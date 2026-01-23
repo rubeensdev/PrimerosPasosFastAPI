5f2a97e8c41158bc19736d4b283c6143b6d3d85a
Ruben <rubeensdev@gmail.com>
2026-01-23 19:43:42 +0100

    correcion backend y requirements

--- backend/main.py
+++ backend/main.py
@@ -13,8 +13,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",   
-        "http://localhost:8080", 
+        "*", 
     ],
     allow_credentials=True,
     allow_methods=["*"],
